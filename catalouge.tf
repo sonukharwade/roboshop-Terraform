ca56ab2e3d874d2811e8c2035bba918449cc9263
kharw <kharwadesonu95@gmail.com>
2025-11-04 20:32:03 +0530

    roboshop-terraform

---
+++ catalouge.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "catalouge" {
+  ami  = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0a707cc6121b14b08"]
+
+  tags = {
+    Name = "catalouge"
+  }
+}
+
+
+
+resource "aws_route53_record" "catalouge" {
+zone_id = "Z09275246XOL4TQU5EUC"
+name = "frontend-dev" # Replace with your domain name
+type = "A"
+ttl = 10
+records = ["aws_instance.catalouge.private_ip"] # Replace with your desired IP address
+}
