adc2b546f5623dc7f7ba6aa5be626c08612d228a
kharw <kharwadesonu95@gmail.com>
2025-11-04 20:52:39 +0530

    roboshop-terraform

--- mongodb.tf
+++ mongodb.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "mongodb" {
 
 resource "aws_route53_record" "mongodb" {
 zone_id = "Z09275246XOL4TQU5EUC"
-name = "frontend-dev" # Replace with your domain name
+name = "mongodb-dev" # Replace with your domain name
 type = "A"
 ttl = 10
 records = [aws_instance.mongodb.private_ip] # Replace with your desired IP address
