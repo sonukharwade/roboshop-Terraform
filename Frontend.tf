f331c421c49679eb78108b9ee8e6f00fc2697125
kharw <kharwadesonu95@gmail.com>
2025-11-04 20:16:34 +0530

    roboshop-terraform

--- Frontend.tf
+++ Frontend.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "Frontend" {
   ami  = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0a707cc6121b14b08"]
 
   tags = {
     Name = "Frontend"
